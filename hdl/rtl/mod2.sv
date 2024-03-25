e1f77e3024748c726b8af2edd64510eb00061ee1
scapekambing <e.firmansyah@outlook.com>
2024-03-24 18:05:08 -0700

    BROKEN: trying to recreate bit reduction mod2

--- hdl/rtl/mod2.sv
+++ hdl/rtl/mod2.sv
@@ -24,7 +24,7 @@ module mod2 #(
   var logic signed [DAC_BW-1:0] dsm_in_extended;
   var logic signed [DAC_BW-1:0] error_z1;
   var logic signed [DAC_BW-1:0] error_z2;
-  var logic signed [DAC_BW-1:0] quantizer;
+  var logic signed [DAC_BW-1:0] quantizer_in;
 
   always_ff @(posedge aclk) begin
     if (~arst_n) begin
@@ -33,22 +33,30 @@ module mod2 #(
       m_axis_data_tdata  <= 1'b0;
       m_axis_data_tvalid <= 1'b0;
     end else if (s_axis_data_tvalid && s_axis_data_tready) begin
+
+      // this meant that the quantizer outputted a 1
       if (m_axis_data_tdata == 1'b1) begin
-        error_z1 <= quantizer + ((-1) <<< DAC_BW - 2);
+        // subtract 1 since this is the difference between the quantizer output and the input
+        error_z1 <= quantizer_in - 1;
+
+        // this meant that the quantizer outputted a 0
+        // we subtract 0 since this is the difference beteween the quantizer output and the input
       end else begin
-        error_z1 <= quantizer + (1 <<< DAC_BW - 2);
+        error_z1 <= quantizer_in - 0;
       end
       error_z2 <= error_z1;
-      m_axis_data_tdata <= ~quantizer[DAC_BW-1];
-      m_axis_data_tvalid <= 1'b1;
+
+      // we are outpting the sign bit
+      // if quantizer_in's MSB is 1 that means we have a negative number, so quantizer outputted a 0
+      // if quantizer_in's MSB is 0 that means we have a positive number, so quantizer outputted a 1
+      m_axis_data_tdata <= ~quantizer_in[WIDTH-1];
     end
   end
 
   always_comb begin
     dsm_in_extended = s_axis_data_tdata;
     s_axis_data_tready = 1'b1;
-
-    quantizer = error_z1 * 2 - error_z2 + dsm_in_extended;
+    quantizer_in = dsm_in_extended + 2 * error_z1 - error_z2;
   end
 
 endmodule
